e9191652dce10b123baedb91336cbefd3f5ad4ae
dashalary <dk2800@nyu.edu>
2020-10-21 13:06:39 -0400

    new edits

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,8 +18,20 @@ class TweetsController < ApplicationController
     end
 
     post '/tweets' do
-        @tweet = Tweet.new
-        @tweets = Tweet.all
+        if logged_in?
+            if params[:content] == ""
+              redirect "/tweets/new"
+            else
+              @tweet = current_user.tweets.build(content: params[:content])
+              if @tweet.save
+                redirect "/tweets/#{@tweet.id}"
+              else
+                redirect "/tweets/new"
+              end
+            end
+        else
+            redirect '/login'
+        end
     end
 
     get '/tweets/:id' do
@@ -32,14 +44,37 @@ class TweetsController < ApplicationController
     end
 
     get '/tweets/:id/edit' do
+        if logged_in?
         @tweet = Tweet.find_by_id(params[:id])
-        erb :edit_tweet
+            if @tweet && @tweet.user == current_user
+            erb :'tweets/edit_tweet'
+            else
+            redirect '/tweets'
+            end
+        else
+        redirect '/login'
+        end
     end
 
     patch '/tweets/:id' do
-        @tweet = Tweet.find_by_id(params[:id])
-        @tweet.update(params[:tweet])
-        redirect '/tweets/#{@tweet.id}'
+        if logged_in?
+            if params[:content] == ""
+                redirect "/tweets/#{params[:id]}/edit"
+            else
+             @tweet = Tweet.find_by_id(params[:id])
+                if @tweet && @tweet.user == current_user
+                    if @tweet.update(content: params[:content])
+                        redirect "/tweets/#{@tweet.id}"
+                    else
+                        redirect "/tweets/#{@tweet.id}/edit"
+                    end
+                else
+                    redirect "/tweets"
+                end
+            end
+        else
+            redirect to '/login'
+        end
     end
 
 
