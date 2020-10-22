ec43ead09406a089869668be2b0366c6ba1bbbdb
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-10-22 15:57:31 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,61 @@
+require 'pry'
 class UsersController < ApplicationController
 
+    get "/signup" do
+        if logged_in?
+            redirect "/tweets"
+        else
+            erb :"users/create_user"
+        end
+    end
+
+    post "/signup" do
+        arr = []
+        username = params[:username]
+        arr << username
+        email = params[:email]
+        arr << email
+        password = params[:password]
+        arr << password
+        if !arr.include?("")
+            user = User.new(username: username, email: email, password: password)
+            if user.authenticate(password)
+                user.save
+                session[:user_id] = user.id
+                redirect "/tweets"
+            end
+        else
+            redirect "/signup"
+        end
+    end
+
+    get "/login" do
+        if logged_in?
+            redirect "/tweets"
+        else
+            erb :"users/login"
+        end
+    end
+
+    post "/login" do
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+
+            session[:user_id] = user.id
+            redirect "/tweets"
+        else
+            redirect "/signup"
+        end
+    end
+
+    get "/logout" do
+        session.clear
+        redirect "/login"
+    end
+
+    get "/users/:slug" do
+        @user = User.find_by_slug(params[:slug])
+        erb :"users/slug"
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
+require 'pry'
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug
+    if @slug
+      @slug
+    else
+      @slug = self.username.split.join("-")
+    end
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find do |user|
+      user.slug == slug
+    end
+  end
+
 end
